725c5179b841ed56857b228cfda6aceaf2f9a9a6
Natalya Taranenko <go.avecanora@gmail.com>
2021-03-15 00:00:55 +0200

    chanhing_theme

--- src/index.js
+++ src/index.js
@@ -1,7 +1,37 @@
 import './styles.css';
 import menuGallery from './menu.json';
 import itemMenu from './templates/menu.hbs';
-const galleryMenu = document.querySelector('.js-menu');
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
+
+const refs = {
+  galleryMenu: document.querySelector('.js-menu'),
+  themeSwitch: document.querySelector('#theme-switch-toggle'),
+};
+
 const markup = itemMenu(menuGallery);
-galleryMenu.insertAdjacentHTML('beforeend', markup);
-console.dir(menuGallery);
+refs.galleryMenu.insertAdjacentHTML('beforeend', markup);
+if (localStorage.getItem('theme') === 'dark-theme') {
+  refs.themeSwitch.checked = true;
+}
+if (localStorage.getItem('theme') != undefined) {
+  addClass(localStorage.getItem('theme'));
+}
+
+refs.themeSwitch.addEventListener('change', themeswitchClick);
+
+function themeswitchClick(evt) {
+  const positionSwith = evt.target;
+  if (positionSwith.checked === true) {
+    localStorage.setItem('theme', Theme.DARK); //темная тема
+  } else {
+    localStorage.setItem('theme', Theme.LIGHT); //светлая тема
+  }
+  addClass(localStorage.getItem('theme'));
+}
+//добавление классов "light-theme" или "dark-theme"
+function addClass(nameTheme) {
+  document.body.className = nameTheme;
+}

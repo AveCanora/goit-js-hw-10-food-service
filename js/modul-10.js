662551baf5301c701400a90ba374094078c8973c
Natalya Taranenko <go.avecanora@gmail.com>
2021-03-14 22:00:28 +0200

    webpack

--- js/modul-10.js
+++
@@ -1,7 +0,0 @@
-// import menuGallery from "./menu.json";
-
-import itemMenu from "../templates/menu.hbs";
-// const galleryMenu = document.querySelector(".js-menu");
-// const markup = itemMenu(menuGallery);
-// galleryMenu.insertAdjacentHTML("beforeend", markup);
-// console.dir(menuGallery);
